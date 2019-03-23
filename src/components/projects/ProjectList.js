a3e5cc40c8de429ad7942d4d9744df848d50c082
hurcs87 <[zoe.hurcombe@holidayextras.com]>
2019-03-23 13:02:28 +0000

    end of lesson 6

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,32 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Zoe</p>
-          <p className="grey-text">23rd March</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Zoe</p>
-          <p className="grey-text">23rd March</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Zoe</p>
-          <p className="grey-text">23rd March</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
