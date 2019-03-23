e1075b9ab3ed3817272b0f499da4e61b66120224
hurcs87 <[zoe.hurcombe@holidayextras.com]>
2019-03-23 12:54:40 +0000

    end of chapter 5

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Zoe</p>
+          <p className="grey-text">23rd March</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Zoe</p>
+          <p className="grey-text">23rd March</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Zoe</p>
+          <p className="grey-text">23rd March</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList;
