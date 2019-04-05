ef5058781a25d9a7b2d9809ece8fddc5757389a2
hurcs87 <[zoe.hurcombe@holidayextras.com]>
2019-04-05 10:08:17 +0100

    end of lesson 14

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
 class CreateProject extends Component {
   state = {
@@ -11,7 +13,8 @@ class CreateProject extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state)
+    // console.log(this.state)
+    this.props.createProject(this.state)
   }
 
   render() {
@@ -36,4 +39,10 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
