dd4d15f18030857c4557848b96cfdb2a671ddb43
hurcs87 <[zoe.hurcombe@holidayextras.com]>
2019-05-03 09:40:34 +0100

    end of lesson 24

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -9,6 +9,16 @@ export const signIn = (credentials) => {
       dispatch({ type: 'LOGIN_SUCCESS' })
     }).catch((err) => {
       dispatch({ type: 'LOGIN_ERROR', err })
+    });
+  }
+}
+
+export const signOut = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    firebase.auth().signOut().then(() => {
+      dispatch({ type: 'SIGNOUT_SUCCESS' })
     })
   }
 }
